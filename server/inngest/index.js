eb9b0952d370ae12be45c8ec9fcdbc114b7b4887
Ayush <ayush.22b1541073@abes.ac.in>
2025-11-01 10:39:49 +0530

    fourth

--- server/inngest/index.js
+++ server/inngest/index.js
@@ -21,7 +21,42 @@ const syncUserCreation = inngest.createFunction(
     }
 )
 
-// Inngest function
+// Inngest function to delete user from database
+const syncUserDeletion = inngest.createFunction(
+    {id: 'delete-user-with-clerk'},
+    {event: 'clerk/user.deleted'},
+    async ({event})=> {
+        const {data} = event
+        await prisma.user.delete({
+            where: {
+                id: data.id,
+            }
+        })
+    }
+)
+
+// Inngest function to update user data in database
+const syncUserUpdation = inngest.createFunction(
+    {id: 'update-user-from-clerk'},
+    {event: 'clerk/user.updated'},
+    async ({event})=> {
+        const {data} = event
+        await prisma.user.update({
+            where: {
+                id: data.id,
+            },
+            data: {
+                email: data?.email_addresses[0]?.email_address,
+                name: data?.first_name + " " + data?.last_name,
+                image: data?.image_url,
+            }
+        })
+    }
+)
 
 // Create an empty array where we'll export future Inngest functions
-export const functions = [];
+export const functions = [
+    syncUserCreation,
+    syncUserDeletion,
+    syncUserUpdation
+];
